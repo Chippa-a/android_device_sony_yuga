56113dd10e54ab9e875ab19c9622ec6e8657d4b6
Chippa-a <vusal1372@gmail.com>
2018-02-09 14:25:33 +0300

    yuga: RR-ify
    Change-Id: I565ab82f51c225caf797de7e0b41440790ec94c5

--- rr.mk
+++ rr.mk
@@ -15,8 +15,8 @@
 # limitations under the License.
 #
 
-# Inherit common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common ResurrectionRemix stuff
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/sony/yuga/device.mk)
@@ -26,7 +26,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifications
-PRODUCT_NAME := lineage_yuga
+PRODUCT_NAME := rr_yuga
 PRODUCT_DEVICE := yuga
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
